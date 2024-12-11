62a99830ca31f105cc9920b0222ca79b2af704f3
Manoj Kumar BV <manojkumarbv.cs23@rvce.edu.in>
2024-12-11 20:22:55 +0530

    Working

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,18 +1,10 @@
 from django.urls import path
 from . import views
 
-from django.contrib import admin
-from django.urls import path, include
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('add/', views.add_task, name='add_task'),
     path('update/<int:task_id>/', views.update_task, name='update_task'),
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
-    path('complete/<int:task_id>/', views.mark_completed, name='mark_completed')  # Ensure this line is present
-]
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('tasks.urls')),
+    path('complete/<int:task_id>/', views.mark_completed, name='mark_completed')
 ]
